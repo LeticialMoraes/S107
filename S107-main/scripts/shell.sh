4c0eb4a77aae7aa2102e2acd728d417dbf0c40e3
LeticialMoraes <108829368+LeticialMoraes@users.noreply.github.com>
2024-03-27 17:09:08 -0300

    Update shell.sh

--- S107-main/scripts/shell.sh
+++ S107-main/scripts/shell.sh
@@ -1,8 +1,16 @@
-ls
-cd Aula-GitHub-Actions
-ls
-echo "AAAHAHAHAAA"
-sudo apt-get install mailutils
-echo "Fim da instalacao"
-echo "Mandando e-mail com mail do linux" | mail -s "a subject" christopher@inatel.br
-# posso fazer o que eu quiser aqui
+
+
+echo "Pipeline executado!"
+
+# Verificando se o e-mail para notificação está disponível como variável de ambiente
+if [ -z "$EMAIL_NOTIFICATION" ]; then
+  echo "A variável de ambiente EMAIL_NOTIFICATION não foi configurada."
+  exit 1
+fi
+
+# Instalando o cliente de e-mail
+sudo apt-get update
+sudo apt-get install -y mailutils
+
+# Enviando um e-mail com informações sobre a execução do pipeline
+echo "Pipeline executado com sucesso!" | mail -s "GitHub Actions Pipeline Notification" "$EMAIL_NOTIFICATION"
